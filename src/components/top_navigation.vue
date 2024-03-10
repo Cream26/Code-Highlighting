f5bae45a1d4448bf829f71fb31a8e4aec2ea4500
Cream26 <zhangtao4901@gmail.com>
2024-03-10 22:19:31 +0800

    Top banner

---
+++ src/components/top_navigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="bg-gray-800 text-white p-4">
+    <div class="container mx-auto flex items-center justify-between">
+      <div class="flex items-center">
+        <img src="../../public/Vector.svg" alt="Logo" class="h-5 w-auto mr-2" />
+        <hr />
+        <div class="text-lg font-bold mx-2">ハイライト</div>
+      </div>
+      <div class="flex justify-end">
+        <el-button type="info">Success</el-button>
+        <el-button type="info">Success</el-button>
+        <el-button type="info">Success</el-button>
+        <el-button type="info">Success</el-button>
+        <span class="divider w-px h-8 bg-slate-200 mx-2 my-0 rounded-md"></span>
+        <el-switch
+          v-model="dark"
+          :active-action-icon="Moon"
+          :inactive-action-icon="Sunny"
+          @change="changeTheme"
+        ></el-switch>
+        <span class="divider w-px h-8 bg-slate-200 mx-2 my-0 rounded-md"></span>
+        <img
+          class="img mx-1 transition-transform transform hover:scale-110 active:scale-90"
+          src="../image/Mail.svg"
+          alt=""
+          @click="sendEmail"
+        />
+        <img
+          class="img mx-1 transition-transform transform hover:scale-110 active:scale-90"
+          src="../image/Github.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Moon, Sunny } from '@element-plus/icons-vue'
+const dark = ref<boolean>(false)
+const changeTheme = () => {
+  const root = document.documentElement
+  dark.value ? (root.className = 'dark') : (root.className = '')
+}
+// const sendEmail = () => {
+//   const recipientEmail: string = 'zhangtao4901@gmail.com'
+//   const subject: string = 'About the highlight'
+//   const body: string = ''
+//   const mailtoLink: any = `mailto:${recipientEmail}?subject=${subject}&body=${body}`
+//   window.open(mailtoLink)
+// }
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/main.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 createApp(App).mount('#app')
